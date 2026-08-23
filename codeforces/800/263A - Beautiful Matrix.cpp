f7c7437314130c8e704e95d949669e9d9e999b33
Yeswanth reddy <yeswanthreddy2801@gmail.com>
2026-08-23 22:09:32 +0530

    Add Beautiful Matrix (800)

---
+++ codeforces/800/263A - Beautiful Matrix.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=5;
+    vector<vector<int>>v(n,vector<int>(n));
+    int ind;
+    int jnd;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>v[i][j];
+            if(v[i][j]==1)
+            {
+                ind=i;
+                jnd=j;
+            }
+            
+        }
+    }
+    cout<<abs(ind-2)+abs(jnd-2);
+
+
+}
