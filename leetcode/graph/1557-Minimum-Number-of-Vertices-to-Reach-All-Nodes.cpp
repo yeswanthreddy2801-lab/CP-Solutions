5b3e1263b6c97e1e2c7cb0b59919fba4c7a12cfd
Yeswanth reddy <yeswanthreddy2801@gmail.com>
2026-08-29 20:33:08 +0530

    Add Minimum-Number-of-Vertices-to-Reach-All-Nodes (graph)

---
+++ leetcode/graph/1557-Minimum-Number-of-Vertices-to-Reach-All-Nodes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int>vis(n);
+        for(int i=0;i<edges.size();i++)
+        {
+            vis[edges[i][1]]++;
+        }
+        vector<int>v;
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]==0)
+            v.push_back(i);
+        }
+        return v;
+    }
+};
