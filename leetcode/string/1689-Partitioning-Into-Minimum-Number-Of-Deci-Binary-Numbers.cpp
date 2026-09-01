3028fabcf76fec51fe6b364d65036238a66750d6
Yeswanth reddy <yeswanthreddy2801@gmail.com>
2026-09-01 19:53:44 +0530

    Add Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers (string)

---
+++ leetcode/string/1689-Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxi=0;
+        for(int i=0;i<n.size();i++)
+        {
+            maxi=max(maxi,n[i]-48);
+        }
+        return maxi;
+    }
+};
